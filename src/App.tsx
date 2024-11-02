7b3d0b12c2f5389b4e2fea098d64959ad94a5696
Some Digital Guys <goncalosantosprofissional@gmail.com>
2024-11-02 12:56:42 +0000

    tailwind & react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+// Pages
+import {
+  EditFeedback,
+  FeedbackDetail,
+  NewFeedback,
+  Roadmap,
+  Suggestions,
+} from "./pages";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={"Landing Page"} />
+      <Route path="/suggestions" element={<Suggestions />} />
+      <Route path="/feedback-detail" element={<FeedbackDetail />} />
+      <Route path="/new-feedback" element={<NewFeedback />} />
+      <Route path="/edit-feebdack" element={<EditFeedback />} />
+      <Route path="/roadmap" element={<Roadmap />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Suggestions } from "./Suggestions";
+export { default as FeedbackDetail } from "./FeedbackDetail";
+export { default as NewFeedback } from "./NewFeedback";
+export { default as EditFeedback } from "./EditFeedback";
+export { default as Roadmap } from "./Roadmap";
